095f9594993d31bc587440e771b5b2269277c41b
xuguoliangjj <1044748759@qq.com>
2016-03-02 11:25:56 +0800

    ueditor莫名其妙丢了

---
+++ vendor/xuguoliangjj/yii2-ueditor/UeditorAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xuguoliang
+ * Date: 2015/7/27
+ * Time: 13:11
+ */
+namespace xuguoliangjj\ueditor;
+
+use yii\web\AssetBundle;
+
+class UeditorAsset extends AssetBundle
+{
+    public $sourcePath = '@xuguoliangjj/ueditor/assets';
+
+    public $css = [
+
+    ];
+    public $js = [
+        'ueditor/ueditor.config.js',
+        'ueditor/ueditor.all.min.js',
+        'ueditor/lang/zh-cn/zh-cn.js'
+    ];
+    public $depends = [
+
+    ];
+}
